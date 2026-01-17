6cf0b3ff9b3424c511350f28a20890de7b360969
Mikhail <mikhail.manco.dev@gmail.com>
2026-01-17 15:07:46 -0500

    create dedicated db module for connection pooling.

---
+++ src/db/index.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
+import * as schema from "./schema.js";
+import { Pool } from "pg";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in .env file.");
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db: NodePgDatabase<typeof schema> = drizzle(pool, { schema });
