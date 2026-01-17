c956fc31f9792536fd7036773926353d447528d5
Mikhail <mikhail.manco.dev@gmail.com>
2026-01-17 14:31:25 -0500

    refactor(server): add explicit type annotations and ensure PORT is number.

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,21 @@
 import "dotenv/config";
-import express, { Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import postsRoutes from "./routes/posts.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app: Application = express();
+const PORT: number = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
 app.use("/posts", postsRoutes);
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response): Response => {
   return res.send("Welcome to my Blogging Platform API");
 });
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, (): void => {
   console.log(`Server running on PORT ${PORT}`);
 });
