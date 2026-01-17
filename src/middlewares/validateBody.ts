ad3592989d6872c055ab95fd9a76c6f6a584a77f
Mikhail <mikhail.manco.dev@gmail.com>
2026-01-17 18:14:27 -0500

    refactor(validateBody.ts): add explicit type annotations.

--- src/middlewares/validateBody.ts
+++ src/middlewares/validateBody.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from "express";
-import { z } from "zod";
+import { Request, Response, NextFunction, RequestHandler } from "express";
+import { ZodObject } from "zod";
 
 // This is a factory function, it must have req, res and next object as parameters.
 // Otherwise, Express won't consider it as a valid middleware.
-export const validateBody = (schema: z.ZodObject) => {
+export const validateBody = (schema: ZodObject): RequestHandler => {
   return (req: Request<{ id?: number }>, res: Response, next: NextFunction) => {
     schema.parse(req.body);
     next();
