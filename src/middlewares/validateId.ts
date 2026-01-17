0e7f40bfeb21d8cade85711b124c80171760d9e4
Mikhail <mikhail.manco.dev@gmail.com>
2026-01-17 01:52:03 -0500

    Created the validateId middleware.

---
+++ src/middlewares/validateId.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { eq } from "drizzle-orm";
+import { postsTable } from "../db/schema.js";
+
+const db = drizzle(process.env.DATABASE_URL!);
+
+export const validateId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<Response | void> => {
+  const id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Invalid post id." });
+  }
+
+  const [post] = await db
+    .select()
+    .from(postsTable)
+    .where(eq(postsTable.id, id));
+
+  if (post) {
+    req.post = post;
+    return next();
+  } else {
+    return res.status(404).json({ error: "Post not found." });
+  }
+};
