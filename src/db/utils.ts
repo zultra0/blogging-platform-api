a8093212b822e585d130adf72701da40d0dea18a
Mikhail <mikhail.manco.dev@gmail.com>
2026-01-17 23:19:35 -0500

    Added partial matching to FTS.

--- src/db/utils.ts
+++ src/db/utils.ts
@@ -7,5 +7,21 @@ export const ftsSearch = (
   lang = "spanish",
 ): SQL => {
   const concatenatedColumns: SQL = sql.join(columns, sql` || ' ' || `);
-  return sql`to_tsvector(${lang}, ${concatenatedColumns}) @@ plainto_tsquery(${lang}, ${term})`;
+  const formattedTerm = term
+    .trim()
+    .split(/\s+/)
+    .filter(Boolean)
+    .map((word) => `${word}:*`)
+    .join(" & ");
+
+  return sql`to_tsvector(${lang}, ${concatenatedColumns}) @@ to_tsquery(${lang}, ${formattedTerm})`;
 };
+
+/*
+Example of final transformation
+If the user types: " Node Express "
+1. Trim: "Node Express"
+2. Split: ["Node", "Express"]
+3. Map: ["Node:*", "Express:*"]
+4. Join: "Node:* & Express:*"
+*/
