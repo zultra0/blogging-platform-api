aa225da5fb7da681982ecf9569d690dcd7e0d386
Mikhail <mikhail.manco.dev@gmail.com>
2026-01-20 14:00:05 -0500

    chore: remove unnecessary complexity from the database connection code.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,8 @@
 import "dotenv/config";
-import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
-import * as schema from "./schema.js";
-import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined in .env file.");
 }
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-export const db = drizzle(pool, { schema });
+export const db = drizzle(process.env.DATABASE_URL);
